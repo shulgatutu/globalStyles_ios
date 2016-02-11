41ca36c2bb0102d9deba9e0a65925e3bceccfde1
Шульга Александр <shulga@tutu.ru>
2016-02-11 10:35:01 +0300

    Initial Commit

---
+++ TestDesign/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestDesign
+//
+//  Created by Шульга Александр on 11.02.16.
+//  Copyright © 2016 Шульга Александр. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
